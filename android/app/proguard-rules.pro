1b845a735c0284d7c8e838508326d6a7c90d9758
orej-support <orej12341234@gmail.com>
2025-09-01 11:46:48 +0300

    Delete android directory

--- android/app/proguard-rules.pro
+++
@@ -1,3 +0,0 @@
-# Keep SLF4J logging classes to avoid R8 missing class errors
--keep class org.slf4j.** { *; }
--dontwarn org.slf4j.**
--- android/app/src/main/kotlin/com/example/yousef/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.yousef
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
